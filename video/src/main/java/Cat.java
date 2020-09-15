2c1534045d0a429e9df7bbf6fc5a65eb2eb29155
DallasG19 <70603629+DallasG19@users.noreply.github.com>
2020-09-15 12:24:47 -0400

    Add files via upload

---
+++ video/src/main/java/Cat.java
@@ -0,0 +1,9 @@
+public class Cat extends Main{
+    @Override
+    public void makeSound(){
+        System.out.println("meow");
+    }
+    public void scratch(){
+        System.out.println("I am a cat. I scratch things.");
+    }
+}
---
+++ video/src/main/java/Main.java
@@ -0,0 +1,90 @@
+public class Main {
+    /*
+    public static void main(String[] args) {
+        System.out.println("Enter your name.");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        greetUser(name);
+    }
+    public static void greetUser(String name){
+        System.out.println("Hi there, " + name);
+    }*/
+/////////////////////////////////////////////////////////////////
+   /* double length;
+    double width;
+    public Main(double length, double width){
+        this.length = length;
+        setWidth(width);
+    }
+
+    double getLength(){
+        return length;
+    }
+    void setLength(double length){
+        this.length = length;
+    }
+    double getWidth(){
+        return width;
+    }
+    void setWidth(double width){
+        this.width = this.width;
+    }
+
+    double calcualatePerimeter(){
+        return (2 * length) + (2 * length);
+    }
+    double calculateArea(){
+        return length*width;
+    }
+
+    */
+///////////////////////////////////////////////////////////////
+    /*public static void generateNumbers(){
+        for (int i = 0; i < length; i++){
+            int randomNumber;
+            do {
+                randomNumber = random.nextInt(MAX_TICKET_NUMBER) + 1;
+            }while(search(ticket, randomNumber));
+            ticket[i] = randomNumber;
+        }
+return ticket;
+    }
+    public static boolean search(int[] ticket){
+        for (int value : array){
+            if (value == numberToSearchFor){
+                return true;
+            }
+        }
+
+    }
+    */
+///////////////////////////////////////////////////////////////////
+//CHAPTER 10
+    /*
+    public static void main(String[] args) {
+        Dog rocky = new Dog();
+        rocky.fetch();
+        rocky.makeSound();
+        feed(rocky);
+
+        Animal sasha = new Dog();
+        sasha.makeSound();
+        feed(sasha);
+
+        sasha = new Cat();
+        sasha.makeSound();
+        feed(sasha);
+
+    }
+
+    public static void feed(Animals[] animals) {
+if (animal istanceof Dog){
+            System.out.println("here's your dog food");
+        }
+else if (animals instanceof Cat){
+            System.out.println("here's your cat food");
+        }
+    }
+
+     */
+}
